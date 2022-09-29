c6d10fe6f3ede0ba9e36d3f24b2d369a039f3670
Marianne Garrido <64554585+Marianne-90@users.noreply.github.com>
2022-09-29 21:45:32 +0000

    uppdate

--- Main.java
+++ Main.java
@@ -1,5 +1,28 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    int resultadoSuma = suma( 1, 2, 3);
+    Coche miCoche = new Coche();
+
+    miCoche.SumarPuerta();
+    miCoche.SumarPuerta();
+    miCoche.SumarPuerta();
+    miCoche.SumarPuerta();
+    
+    System.out.println( "El resultado de la suma es: " + resultadoSuma );
+    System.out.println( "Mi coche tiene: " + miCoche.puertas + " puertas" );
+    
+  }
+
+  public static int suma( int a, int b, int c){
+    return a + b + c;
   }
 }
+
+class Coche {
+  public int puertas = 0;
+
+  public void SumarPuerta(){
+    this.puertas ++;
+  }
+
+  }
